4e25477ae051f082e8c61972d45a83f1220dcfc3
miju <alwn8918@gmail.com>
2025-02-24 04:51:16 +0900

    ✨ Feat : 온보딩 페이지 기본 UI 설정

--- src/pages/Home/Onboarding.jsx
+++ src/pages/Home/Onboarding.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import * as O from "../../styles/Home/OnboardingStyle";
+
+import brownLogo from "../../assets/images/brownLogo.png";
 
 function Onboarding() {
-    return <div>Onboarding</div>;
+	return (
+		<O.Container>
+			<O.Logo src={brownLogo} />
+			<O.Footer>
+				<O.FooterGuide>
+					*모아북에 첨부되는 URL을 비롯한 모든 작성물 내용의 권리는
+					작성자에게 있으며, 작성자의 동의 없는 무단 배포 및 사용은
+					금지됩니다.
+				</O.FooterGuide>
+				<O.FooterTeam>@KDT_Cloud_Team</O.FooterTeam>
+			</O.Footer>
+		</O.Container>
+	);
 }
 
 export default Onboarding;
--- src/styles/Home/OnboardingStyle.jsx
+++ src/styles/Home/OnboardingStyle.jsx
@@ -1,5 +1,45 @@
 import styled from "styled-components";
 
-export const container = styled.div`
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	height: 100vh;
+	width: 100vw;
+	max-width: 100%; /* 부모보다 커지지 않도록 설정 */
+
 	background: linear-gradient(180deg, #fff 0%, #bb927f 100%);
+`;
+
+export const Logo = styled.img`
+	margin-top: 40px;
+	width: 194.5px;
+	height: 50px;
+`;
+
+export const Footer = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	gap: 70px;
+
+	margin-top: auto;
+	padding: 20px 0;
+
+	width: 100%;
+
+	background: #a5735d;
+`;
+
+export const FooterGuide = styled.p`
+	color: #fff;
+	text-align: center;
+	font-size: 16px;
+`;
+
+export const FooterTeam = styled.p`
+	color: #fff;
+	text-align: center;
+	font-size: 20px;
 `;
